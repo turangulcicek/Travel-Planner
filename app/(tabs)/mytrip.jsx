35703e336879ac694214b675ad2bdeb0350bf6be
turangulcicek <turangulcicek0@gmail.com>
2024-10-10 22:15:18 +0300

    triplist in progress

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
-import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
 import { useRouter } from "expo-router";
+import { auth, db } from "./../../configs/FirebaseConfig";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import UserTripList from "../../components/MyTrips/UserTripList";
+
 export default function Mytrip() {
   const [userTrips, setuserTrips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    user && GetMyTrips();
+  }, [user]);
+  const user = auth.currentUser;
+  const GetMyTrips = async () => {
+    setLoading(true);
+    setuserTrips([]);
+    const q = query(
+      collection(db, "UserTrips"),
+      where("userEmail", "==", user?.email)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      setuserTrips([...userTrips, doc.data()]);
+    });
+    setLoading(false);
+  };
+
   const router = useRouter();
   const handleClick = () => {
     router.push("create-trip/search-place");
@@ -20,7 +51,8 @@ export default function Mytrip() {
           <Ionicons name="add-circle" size={40} color="black" />
         </Pressable>
       </View>
-      {userTrips?.length === 0 ? <StartNewTripCard /> : null}
+      {loading && <ActivityIndicator size={"large"} color={"black"} />}
+      {userTrips?.length === 0 ? <StartNewTripCard /> : <UserTripList />}
     </View>
   );
 }
---
+++ components/MyTrips/UserTripList.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function UserTripList() {
+  return (
+    <View>
+      <Text>UserTripList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
