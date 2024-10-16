4693ed64bc4b65613d3ac7ab5ef7254fd9f37dd6
turangulcicek <turangulcicek0@gmail.com>
2024-10-16 11:53:49 +0300

    tripslist in progress

--- components/MyTrips/UserTripList.jsx
+++ components/MyTrips/UserTripList.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function UserTripList() {
+export default function UserTripList({ userTrips }) {
   return (
     <View>
       <Text>UserTripList</Text>
